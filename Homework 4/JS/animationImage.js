0cacc25234bc72947f06b752f591d6ff093f0cc1
CJ-Griffin0826 <cg233293@umconnect.umt.edu>
2025-02-18 21:57:41 -0700

    Homework 4 done!
    lets goo

---
+++ Homework 4/JS/animationImage.js
@@ -0,0 +1,22 @@
+class animationImages{
+
+
+    // constructor
+    constructor(fileName, x, y, w, h){
+        this.fileName = fileName
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.boy = this.loadAnimation()
+    }
+
+    loadAnimation(){
+        return loadImage(this.fileName)
+    }
+
+    drawAnimation(){
+        image(this.boy, this.x, this.y, this.w, this.h)
+    }
+
+}
